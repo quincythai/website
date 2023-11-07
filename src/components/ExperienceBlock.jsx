bfa65503fdc3764de0ba7b486d86365d8da5f6f3
Quincy Thai <quincythai50@icloud.com>
2023-11-06 21:30:02 -0800

    Begin experience page

--- src/components/ExperienceBlock.jsx
+++ src/components/ExperienceBlock.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 
-const Experience = () => {
+const ExperienceBlock = ({ title, subtext, description }) => {
   return (
-    <div>
-      
+    <div className="flex flex-col gap-2">
+      <p className="text-xl font-bold">{title}</p>
+      <p className="text-xs">{subtext}</p>
+      <p className="description">{description}</p>
     </div>
   );
 };
 
-export default Experience;
+export default ExperienceBlock;
--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -4,7 +4,12 @@ import ExperienceBlock from "../components/ExperienceBlock";
 const Experience = () => {
   return (
     <div>
-      <ExperienceBlock />
+      <h1 className="py-6 text-center text-5xl font-bold">Experience</h1>
+      <ExperienceBlock
+        title="University of California, Berkeley"
+        subtext="2023 to 2025. B.A. in Computer Science"
+        description="Relevant coursework: Data Structures, Foundations of Data ScienceS"
+      />
     </div>
   );
 };
