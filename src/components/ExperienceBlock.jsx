5b404278d6f403fbcd56170bf8943f7910260e83
Quincy Thai <quincythai50@icloud.com>
2023-11-06 21:20:59 -0800

    Remove block

--- src/components/ExperienceBlock.jsx
+++ src/components/ExperienceBlock.jsx
@@ -1,10 +1,9 @@
 import React from "react";
-import ExperienceBlock from "../components/ExperienceBlock";
 
 const Experience = () => {
   return (
     <div>
-      <ExperienceBlock />
+      
     </div>
   );
 };
